68ee78f0bedb31f67f2128f7886e0e3cdf06f68d
Sebastian Waz <sebwaz@g.ucla.edu>
2016-09-17 18:24:42 -0700

    Basic map drawing
    Set up functionality to create a map with random values and visualize it
    according to value in each space

--- automaton.cpp
+++ automaton.cpp
@@ -1,40 +1,104 @@
 #include <iostream>
+#include <time.h>
 #include <glut.h>
 using namespace std;
 
-GLfloat gfPosX = 0.0;
-GLfloat gfDeltaX = .0001;
+/////////////
+// GLOBALS //
+/////////////
 
-void Draw() {
+/* TSP SPEC */
+static const int S_GRID = 50; // assume square grid for now
+static const int NUM_PT = 6;  // TODO: colorization currently only handles 6 points 
+static int GRID[S_GRID][S_GRID];
+static GLfloat unit_span = 0.3 / S_GRID;
+
+
+
+/////////////
+// GL FXNS //
+/////////////
+
+// draw function called by glutMainLoop()
+void Draw()
+{
+	/* SPECIFY COLOR */
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_LINES);
-	glVertex3f(gfPosX, 0.25, 0.0);
-	glVertex3f(1.0 - gfPosX, 0.75, 0.0);
-	glEnd();
-	glFlush();
-	gfPosX += gfDeltaX;
-	if (gfPosX >= 1.0 || gfPosX <= 0.0) {
-		gfDeltaX = -gfDeltaX;
+
+	/* DRAW MAP */
+	for (int i = 0; i < S_GRID; i++)
+	{
+		for (int j = 0; j < S_GRID; j++)
+		{
+			// color based on grid value
+			cout << GRID[i][j];
+			switch(GRID[i][j])
+			{ 
+				case 0: glColor3f(1.0, 0.0, 0.0); break;
+				case 1: glColor3f(0.0, 1.0, 0.0); break;
+				case 2: glColor3f(0.0, 0.0, 1.0); break;
+				case 3: glColor3f(1.0, 1.0, 0.0); break;
+				case 4: glColor3f(0.0, 1.0, 1.0); break;
+				case 5: glColor3f(1.0, 0.0, 1.0); break;
+			}
+
+			// draw unit
+			glBegin(GL_POLYGON);
+			glVertex3f(0.1 + unit_span*(i),   0.35 + unit_span*(j),   0.0);
+			glVertex3f(0.1 + unit_span*(i+1), 0.35 + unit_span*(j),   0.0);
+			glVertex3f(0.1 + unit_span*(i),   0.35 + unit_span*(j+1), 0.0);
+			glVertex3f(0.1 + unit_span*(i+1), 0.35 + unit_span*(j+1), 0.0);
+			glEnd();
+		}
 	}
+	glFlush();
+}
+
+// called when Draw() returns
+void Idle()
+{
+	// calls Draw()
 	glutPostRedisplay();
 }
 
-void Initialize() {
+
+// called before glutMainLoop() to specify properties
+void Initialize()
+{
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 
-int main(int iArgc, char** cppArgv) {
+
+
+//////////
+// MAIN //
+//////////
+
+int main(int iArgc, char** cppArgv)
+{
+	// initialize map with random values
+	srand(time(NULL));
+	for (int i = 0; i < S_GRID; i++)
+	{
+		for (int j = 0; j < S_GRID; j++)
+		{
+			GRID[i][j] = rand() % NUM_PT;
+		}
+	}
+	
+	// run glut
 	glutInit(&iArgc, cppArgv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(250, 250);
-	glutInitWindowPosition(200, 200);
-	glutCreateWindow("XoaX.net");
+	glutInitWindowSize(600, 600);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("TSP Automaton");
 	Initialize();
 	glutDisplayFunc(Draw);
+	glutIdleFunc(Idle);
 	glutMainLoop();
 	return 0;
 }
